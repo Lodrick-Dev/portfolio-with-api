05184eab9abcad87a81fd9e0e33df9eb4ef42a23
Lodrick <dev.frenchlod@gmail.com>
2023-10-29 08:44:55 +0100

    ✅ Description dans section projets

--- src/usables/Projets.jsx
+++ src/usables/Projets.jsx
@@ -4,7 +4,8 @@ import { styled } from "styled-components";
 const Projets = (props, ref) => {
   return (
     <StyledProjets id="projet-composant" ref={ref}>
-      Projets
+      Pas encore de projets digne de ce nom 😆 mais j'y travaille. Ce port
+      Portfolio est un MERN PROJET 😎 non responsive pour l'instant 😥
     </StyledProjets>
   );
 };
